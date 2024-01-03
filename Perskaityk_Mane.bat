d752e52e2dfd216db847c27f8e8e412701890d03
Adas <100019532+adasjusk@users.noreply.github.com>
2024-01-03 12:36:16 +0200

    Add files via upload

---
+++ Perskaityk_Mane.bat
@@ -0,0 +1,21 @@
+Orange PC Booster License
+
+Autorių teisės (c) 2024 TheAdas 2
+
+Leidimas suteikiamas nemokamai bet kuriam asmeniui, gaunančiam kopiją
+šios programinės įrangos ir susijusių dokumentų failų (toliau – Programinė įranga), spręsti
+programinėje įrangoje be apribojimų, įskaitant be apribojimų teises
+naudoti, kopijuoti, keisti, sujungti, skelbti, platinti, sublicencijuoti ir (arba) parduoti
+Programinės įrangos kopijas ir leisti asmenims, kuriems ši programinė įranga priklauso
+parengta tai padaryti, laikantis šių sąlygų:
+
+Aukščiau pateiktas pranešimas apie autorių teises ir šis pranešimas apie leidimą turi būti įtrauktas į visus
+programinės įrangos kopijų ar didelių dalių.
+
+PROGRAMINĖ ĮRANGA TEIKIAMA „TOKI, KOKIA YRA“, BE JOKIOS GARANTIJOS, AIŠKIAI ARBA
+NUMANOMAS, ĮSKAITANT, BET JOMIS NEAPSIribojant, GARANTIJAS, DĖL PARDUOTA,
+TINKAMUMAS KONKREČIAM TIKSLUI IR NEPAŽEIDIMAS. JOKIU ATVEJU NEBUS
+AUTORIAI AR AUTORIŲ TEISIŲ TURĖTOJAI ATSAKO UŽ JOKIUS PAREIŠKUS, ŽALĄ AR KITĄ
+ATSAKOMYBĖ, ATSIŽVELGIANT Į SUTARTIES VEIKSMUS, ATSAKOMYBĘ AR KITĄ KITĄ ATSAKOMYBĘ, KILMĘ IŠ
+BE PROGRAMINĖS ĮRANGOS ARBA NAUDOJANT AR KITI SUSIJĘ SU SU
+PROGRAMINĖ ĮRANGA.
