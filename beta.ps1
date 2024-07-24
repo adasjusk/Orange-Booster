de364b052f2acef3d3d53d74e6ef06dc0b258e5d
Adas <100019532+adasjusk@users.noreply.github.com>
2024-07-24 12:48:30 +0300

    Changed Code And Name
    Just Changing When You Run This Program In Powershell

--- beta.ps1
+++ beta.ps1
@@ -3,7 +3,7 @@ $ErrorActionPreference = "Stop"
 [Net.ServicePointManager]::SecurityProtocol = [Net.ServicePointManager]::SecurityProtocol -bor [Net.SecurityProtocolType]::Tls12
 
 $CommonURLPart = 'bostr.bat'
-$DownloadURL1 = 'https://raw.githubusercontent.com/adasjusk/Orange-Booster/main/' + $CommonURLPart
+$DownloadURL1 = 'https://raw.githubusercontent.com/adasjusk/Orange-Booster/beta/' + $CommonURLPart
 
 $URLs = @($DownloadURL1)
 $RandomURL1 = Get-Random -InputObject $URLs
