08d669d1057a4bd165124ae7445995abeecb22de
Adas <100019532+adasjusk@users.noreply.github.com>
2024-09-16 11:50:42 +0300

    Delete Uninstaller/bloat.bat

--- Uninstaller/bloat.bat
+++
@@ -1,4 +0,0 @@
-@ECHO OFF
-SET ThisScriptsDirectory=%~dp0
-SET PowerShellScriptPath="%ThisScriptsDirectory%bloat.ps1"
-PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& {Start-Process PowerShell -ArgumentList '-NoProfile -ExecutionPolicy Bypass -File ""%PowerShellScriptPath%""' -Verb RunAs}";
