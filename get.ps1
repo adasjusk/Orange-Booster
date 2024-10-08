f8f949cee26f473fa3dcb9f3bec96cc76f8dbc9c
Adas <100019532+adasjusk@users.noreply.github.com>
2024-10-08 13:13:35 +0300

    Updated For ASCII Support

--- get.ps1
+++ get.ps1
@@ -5,27 +5,29 @@ $ErrorActionPreference = "Stop"
 $CommonURLPart = 'bostr.bat'
 $DownloadURL1 = 'https://raw.githubusercontent.com/adasjusk/Orange-Booster/main/' + $CommonURLPart
 
-$URLs = @($DownloadURL1)
-$RandomURL1 = Get-Random -InputObject $URLs
-
 try {
-    $response = Invoke-WebRequest -Uri $RandomURL1 -UseBasicParsing
-}
-catch {
-    Write-Host "Failed to download the script from $RandomURL1"
+    $response = Invoke-WebRequest -Uri $DownloadURL1 -UseBasicParsing
+} catch {
+    Write-Host "Failed to download the script from $DownloadURL1"
+    Write-Host "Error: $_"
     exit 1
 }
 
 $rand = Get-Random -Maximum 99999999
 $isAdmin = ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)
-$FilePath = if ($isAdmin) { "$env:SystemRoot\Temp\bostr_$rand.bat" } else { "$env:TEMP\bostr_$rand.bat" }
+
+$AppDataPath = Join-Path -Path $env:APPDATA -ChildPath 'InerJava-Programs'
+if (-not (Test-Path -Path $AppDataPath)) {
+    New-Item -Path $AppDataPath -ItemType Directory | Out-Null
+}
+$FilePath = Join-Path -Path $AppDataPath -ChildPath "bostr_$rand.bat"
 
 $ScriptArgs = "$args "
 $prefix = "@::: $rand `r`n"
-$content = $prefix + $response
-Set-Content -Path $FilePath -Value $content
+$content = $prefix + $response.Content
 
-Start-Process -FilePath "cmd.exe" -ArgumentList "/c $FilePath $ScriptArgs" -Verb RunAs -Wait
+# Use UTF8 encoding to preserve ASCII characters
+Set-Content -Path $FilePath -Value $content -Encoding UTF8
 
-$FilePaths = @("$env:TEMP\bostr*.bat", "$env:SystemRoot\Temp\bostr*.bat")
-foreach ($FilePath in $FilePaths) { Get-Item $FilePath | Remove-Item }
+# Start the batch file as an administrator
+Start-Process -FilePath "cmd.exe" -ArgumentList "/c `"$FilePath`" $ScriptArgs" -Verb RunAs -Wait
